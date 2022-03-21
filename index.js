7fee74fa2fd8aafe229b35ba55ea57a33133f9b2
Pedro Lima <pedro.oficial1969@gmail.com>
2022-03-21 10:56:48 -0300

    feat(requisito-1): adiciona requisito 1 completo.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +8,22 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const fileJson = () => fs.readFile('./talker.json', 'utf-8')
+  .then((data) => JSON.parse(data));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const dataTalker = await fileJson();
+
+  try {
+    res.status(200).json(dataTalker);
+  } catch (error) {
+    res.status(200).json([]);
+  }
 });
 
 app.listen(PORT, () => {
